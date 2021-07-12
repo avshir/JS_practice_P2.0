0f182a244a22b2ec74ae83cb72bf1fac88436f2e
Anna Sh <av.shirinskaya@gmail.com>
2021-07-12 14:33:39 +0300

    add folder test

---
+++ test/js/script.js
@@ -0,0 +1,23 @@
+// 'use strict';
+
+
+// alert(32);
+console.log(typeof 32);
+console.log(typeof (confirm));
+let a = 5;
+let b = 10;
+let name = 'Вася';
+let num = 'a';
+console.log(`Ваше имя  ${name}`);
+console.log(+num);
+console.log(Number(true));
+console.log(a++);
+console.log(a);
+
+b++;
+console.log(b);
+console.log(undefined + 1);
+
+
+
+
